79ae719689d8c808bb3667427f0c823e2b80b05a
skim2536 <skim2536@uwo.ca>
2023-11-07 23:02:58 -0500

    quizapi header, and class. unfinished. issue with api calling and external lib

--- src/quizapi.cpp
+++ src/quizapi.cpp
@@ -1,3 +1,4 @@
+//#include <curl/curl.h>
 #include <string>
 using namespace std;
 
@@ -8,6 +9,8 @@ class quizapi {
 
     string urlTemplate = "https://opentdb.com/api.php?amount=%s&category=%s&difficulty=%s", questionNumber, category, difficulty;
 
+
+    /*
     //NOT SURE. TESTING. IF USING CURL LIBRARY, NEED TO ADD EXTERNAL. DO NOT USE
     static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) {
         size_t newLength = size*nmemb;
@@ -19,6 +22,7 @@ class quizapi {
             return 0;
         }
     }
+    */
 
     /* ????????????? AGAIN FOR CURL LIB. DO NOT USE YET. TESTING
     curl_global_init(CURL_GLOBAL_DEFAULT);
@@ -41,4 +45,40 @@ class quizapi {
         // TODO: Parse the JSON response stored in 'readBuffer' and add the questions to the 'questions' vector
     */
 
+   // testfunction for trivia calling. not working. fix url directing???
+
+   /*
+       void GetTriviaQuestions() {
+        CURL* curl;
+        CURLcode res;
+        std::string readBuffer;
+
+        curl_global_init(CURL_GLOBAL_DEFAULT);
+        curl = curl_easy_init();
+
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url_.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+            res = curl_easy_perform(curl);
+
+            if(res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            }
+
+            curl_easy_cleanup(curl);
+        }
+
+        curl_global_cleanup();
+
+        // Write to file
+        std::ofstream outFile("quizoutput.json");
+        outFile << readBuffer;
+        outFile.close();
+    }
+   */
+
+    //array containing qaset
+
+    
 };
